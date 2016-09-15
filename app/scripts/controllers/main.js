6c0deec24e66c8768f33584fb68fd84492289476
bld_aalarcon <bld_aalarcon@uolinc.com>
2016-09-15 16:35:30 -0300

    yo angular installation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pocDynamicFormApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pocDynamicFormApp
+ */
+angular.module('pocDynamicFormApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
